54858eeb09149d76bf2209a73e91ba9a3ea4c77b
Vinni-Cedraz <planetexpress0101@gmail.com>
2022-10-02 13:02:17 -0300

    turns battle updated and poem added

---
+++ waiting42.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+sleep 1
+echo -e	"\n\n\n"
+
+echo "sometimes when we're walking"
+sleep 3
+echo "_______________we walk alone"
+sleep 3
+echo "and sometimes when we're____"
+echo "talking_____________________"
+sleep 3 
+echo "we talk alone_______________"
+sleep 3
+echo "but sometimes when we're alo"
+echo "alone_______________________"
+sleep 3
+echo "we're only waiting for someo"
+echo "someone_____________________"
+sleep 3
+echo "and sometimes while we're wa"
+echo "waiting_____________________"
+sleep 3 
+echo "____________________________"
+echo "_________we_________________"
+sleep 1
+echo "_____________are____________"
+sleep 1
+echo "__________________not_______"
+sleep 1
+echo "_______________________alone"
+echo -e "\n\n\n"
