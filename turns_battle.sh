068c81450bdb993d25272aaf71b0f21f00952110
Vinni <planetexpress0101@gmail.com>
2022-10-02 13:02:17 -0300

    turns battle updated and poem added

--- turns_battle.sh
+++ turns_battle.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
-echo "elder:                -welcome to the man who calls himself \"hero\""
-sleep 3
-echo "narrator:             -with a scornful laughter, the elder asks you what is your life path"
-sleep 3
-echo "narrator:             -answer according to the options below:
+sleep 2
+echo "elder:      -welcome to the man who calls himself \"hero\""
+sleep 2
+echo "narrator:   -with a scornful laughter, the elder asks you what is your life path"
+sleep 2
+echo "narrator:   -answer 1, 2 or 3 according to the options below:
 1 - samurai
 2 - prophet
 3 - tanker"
@@ -53,22 +54,27 @@ read hero_odds
 beast_odds=$(( $RANDOM % 2))
 
 if [[ $beast_odds == $hero_odds ]]; then
-      echo "the $type hero casts $skill on the beast, for a total of $attack damage points."
-      sleep 3
+      # the next message will be echoed on the green ansi color:
+      echo -e "\e[32mHero casts $skill on the beast for a total of $attack damage points\e[0m"
       beast_hp=$(( $beast_hp - $attack))       
-      echo "the beast's hp now is $beast_hp"
+      echo -e "\e[32mthe beast's hp now is $beast_hp\e[0m"
        
       if [[ $beast_hp = 0 ]]; then
             beast_hp=20
             true_hp=$hero_hp
-            echo "you have slayed the beast, hero, but I bet you won't be as lucky next time"
+            # the next message will be echoed on the cyan blue ansi color:
+            echo -e "            \e[36mYou've defeated the beast. 
+            Your hp has been recovered and you can now keep going forward... 
+            but I bet you won't be as lucky next time\e[0m"
             sleep 3
       fi
       else
-          echo "beast angrily stomps the hero for a total of $beast_attack damage points"
+          # the next message will be echoed on the red ansi color:
+          echo -e "\e[31mThe beast angrily stomps the hero for a total of $beast_attack damage points\e[0m"
+
           sleep 3
           true_hp=$(( $true_hp - $beast_attack ))
-          echo "hero's hp is now $true_hp"
+          echo -e "\e[31mhero's hp is now $true_hp\e[0m"
           
           if [[ $true_hp = 0 ]]; then
               echo "you died"
